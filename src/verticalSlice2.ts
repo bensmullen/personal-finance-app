3186edd9094db70ef7a9e0c158b1bef0cba5c429
bensmullen <49297081+bensmullen@users.noreply.github.com>
2026-09-01 13:42:39 -0400

    Complete PR 9 semantic audit fixes

--- src/verticalSlice2.ts
+++ src/verticalSlice2.ts
@@ -1,1 +1,22 @@
-export * from "./simulation/verticalSlice2.js";
+export {
+  runVerticalSlice2,
+  createVerticalSlice2PrimitiveId,
+} from "./simulation/verticalSlice2.js";
+export type {
+  HouseholdId,
+  PersonId,
+  AccountId,
+  LiabilityId,
+  IncomeId,
+  ExpenseId,
+  EventId,
+  PrimitiveInstanceId,
+  ScheduledCashFlowEvent,
+  RecurringIncomeStream,
+  RecurringExpenseStream,
+  VerticalSlice2Input,
+  VerticalSlice2RunInput,
+  ProjectedCashFlowOccurrence,
+  VerticalSlice2PeriodResult,
+  VerticalSlice2RunResult,
+} from "./simulation/verticalSlice2.js";
